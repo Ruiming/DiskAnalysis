f28b5c5cd965ddb52786b7f92d21a91cb40f2867
ruiming <ruiming.zhuang@gmail.com>
2016-08-17 00:51:39 +0800

    count size, folderCount, fileCount of each folder

--- main.js
+++ main.js
@@ -27,6 +27,9 @@ var drivelist = require('drivelist');
                 path: '/home/ruiming/Dropbox/',
                 file: [],
                 folder: [],
+                size: 0,
+                fileCount: 0,
+                folderCount: 0
             };
             search(tree).then(tree => {
                 console.log(tree);
@@ -49,11 +52,17 @@ var drivelist = require('drivelist');
                                     } else {
                                         data.path = tree.path + fileName;
                                         if (data.isFile()) {
+                                            // 计算文件数，遍历到文件即可父节点加1
+                                            // 注意遍历的终点在这里
+                                            tree.fileCount ++;
+                                            data.fileCount = 1;
                                             tree.file.push(data);
                                         } else {
+                                            data.folderCount = 0;
                                             data.path += '/';
                                             data.folder = [];
                                             data.file = [];
+                                            data.fileCount = 0;
                                             tree.folder.push(data);
                                         }
                                         resolve(data);
@@ -67,12 +76,18 @@ var drivelist = require('drivelist');
                         });
                     }
                 })
-            }).then(datas => {
+            }).then(tree => {
                 return new Promise((resolve, reject) => {
-                    let promises = datas.folder.map(stat => {
+                    let promises = tree.folder.map(stat => {
                         return search(stat);
                     });
-                    Promise.all(promises).then(() => {
+                    Promise.all(promises).then((datas) => {
+                        datas.map(stat => {
+                            if(stat.isDirectory())  tree.folderCount += stat.folderCount || 1;
+                            // 计算容量要等待该层次异步结束才能操作
+                            tree.size += stat.size;
+                            tree.fileCount += stat.fileCount;
+                        });
                         resolve(tree);
                     })
                 });
