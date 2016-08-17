c5f2d338b0924c6657390c168e8a1c31c0f34054
ruiming <ruiming.zhuang@gmail.com>
2016-08-17 18:02:58 +0800

    success get disk info

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var fs = require('original-fs');
 var os = require('os');
 var drivelist = require('drivelist');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 (function() {
     angular
@@ -23,50 +25,53 @@ var drivelist = require('drivelist');
         function analysis(mountpoint) {
             var startTime = new Date().getTime();
             let dist = mountpoint.split(',');
-            if(dist.indexOf('/') !== -1) {
-                // TODO
-                root.path = '/etc/';
-                new Promise((resolve, reject) => {
-                    fs.stat(root.path, (err, data) => {
-                        if(err) {
-                            log.push(err);
-                            reject(err);
-                        } else {
-                            Object.assign(root, data, {file: [], folder: [], size: 0, fileCount: 0, folderCount: 0});
-                            resolve(root);
-                        }
-                    });
-                }).then(root => search(root))
-                    .then(() => {
-                        console.log(root);
-                        var endTime = new Date().getTime();
-                        console.log(endTime - startTime + "ms");
-                        // debugger;
-                        console.log(log);
-                    });
-            }
+            // TODO: multi mountpoint ?
+            root.path = dist[0];
+            new Promise((resolve, reject) => {
+                fs.lstat(root.path, (err, data) => {
+                    if(err) {
+                        log.push(err);
+                        reject(err);
+                    } else {
+                        Object.assign(root, data, {file: [], folder: [], size: 0, fileCount: 0, folderCount: 0});
+                        resolve(root);
+                    }
+                });
+            }).then(root => search(root))
+                .then(() => {
+                    console.log(root);
+                    var endTime = new Date().getTime();
+                    console.log(endTime - startTime + "ms");
+                    // debugger;
+                    console.log(log);
+                });
         }
 
         function search(tree) {
             return new Promise((resolve, reject) => {
                 fs.readdir(tree.path, (err, data) => {
+                    // Use resolve to avoid Promise.all no work
                     if (err) {
-                        // TODO
                         log.push(err);
                         resolve();
                     } else {
                         let promises = data.map(fileName => {
                             return new Promise((resolve, reject) => {
-                                fs.stat(tree.path + fileName, (err, stat) => {
+                                fs.lstat(tree.path + fileName, (err, stat) => {
                                     if (err) {
-                                        // TODO
                                         log.push(err);
                                         resolve();
                                     } else {
                                         stat.path = tree.path + fileName;
-                                        if (stat.isDirectory()) {
-                                            stat.folderCount = 0;
+                                        // ignore directory /proc
+                                        if (stat.path === '/proc') {
+                                            tree.fileCount++;
+                                            stat.fileCount = 1;
+                                            tree.file.push(stat);
+                                            resolve(stat);
+                                        } else if (stat.isDirectory()) {
                                             stat.path += '/';
+                                            stat.folderCount = 0;
                                             stat.folder = [];
                                             stat.file = [];
                                             stat.fileCount = 0;
@@ -105,11 +110,10 @@ var drivelist = require('drivelist');
                             });
                             resolve(tree);
                         }).catch(err => {
-                            debugger;
+                            log.push(err);
                         });
                     });
             })
         }
     }
-
 }());
