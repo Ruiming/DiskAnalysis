bf9f1955ba622f8df568e13977d60d59cb2d0d44
ruiming <ruiming.zhuang@gmail.com>
2016-08-17 13:48:25 +0800

    fix algorithm

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var fs = require('original-fs');
 var os = require('os');
 var drivelist = require('drivelist');
 
@@ -9,10 +9,9 @@ var drivelist = require('drivelist');
 
     MainController.$inject = ['$scope', '$interval'];
 
-    function MainController($scope) {
-
-        var tree = [];
-
+    function MainController($scope, $interval) {
+        var root = {};
+        var log = [];
         $scope.analysis = analysis;
 
         // 获取硬盘信息
@@ -22,48 +21,62 @@ var drivelist = require('drivelist');
         });
 
         function analysis(mountpoint) {
-            // let dist = mountpoint.split(',');
-            let tree = {
-                path: '/home/ruiming/Desktop/New/',
-                file: [],
-                folder: [],
-                size: 0,
-                fileCount: 0,
-                folderCount: 0
-            };
-            search(tree).then(tree => {
-                console.log(tree);
-            });
+            var startTime = new Date().getTime();
+            let dist = mountpoint.split(',');
+            if(dist.indexOf('/') !== -1) {
+                // TODO
+                root.path = '/home/';
+                new Promise((resolve, reject) => {
+                    fs.stat(root.path, (err, data) => {
+                        if(err) {
+                            log.push(err);
+                            reject(err);
+                        } else {
+                            Object.assign(root, data, {file: [], folder: [], size: 0, fileCount: 0, folderCount: 0});
+                            resolve(root);
+                        }
+                    });
+                }).then(root => search(root))
+                    .then(() => {
+                        console.log(root);
+                        var endTime = new Date().getTime();
+                        console.log(endTime - startTime + "ms");
+                        // debugger;
+                        console.log(log);
+                    });
+            }
         }
 
         function search(tree) {
             return new Promise((resolve, reject) => {
                 fs.readdir(tree.path, (err, data) => {
-                    if(err) {
-                        console.log(err);
-                        reject(err);
+                    if (err) {
+                        // TODO
+                        log.push(err);
+                        resolve();
                     } else {
                         let promises = data.map(fileName => {
                             return new Promise((resolve, reject) => {
                                 fs.stat(tree.path + fileName, (err, data) => {
                                     if (err) {
-                                        console.log(err);
-                                        reject(err);
+                                        // TODO
+                                        log.push(err);
+                                        resolve();
                                     } else {
                                         data.path = tree.path + fileName;
-                                        if (data.isFile()) {
-                                            // 文件数计算
-                                            tree.size += data.size;
-                                            tree.fileCount ++;
-                                            data.fileCount = 1;
-                                            tree.file.push(data);
-                                        } else {
+                                        if (data.isDirectory()) {
                                             data.folderCount = 0;
                                             data.path += '/';
                                             data.folder = [];
                                             data.file = [];
                                             data.fileCount = 0;
                                             tree.folder.push(data);
+                                        } else {
+                                            // 文件数计算
+                                            tree.size += data.size;
+                                            tree.fileCount++;
+                                            data.fileCount = 1;
+                                            tree.file.push(data);
                                         }
                                         resolve(data);
                                     }
@@ -76,26 +89,27 @@ var drivelist = require('drivelist');
                     }
                 })
             }).then(tree => {
-                return new Promise((resolve, reject) => {
-                    let promises = tree.folder.map(stat => {
-                        return search(stat);
-                    });
-                    Promise.all(promises).then((datas) => {
-                        datas.map(stat => {
-                            // 文件夹计算
-                            if(stat.isDirectory())  tree.folderCount += stat.folderCount + 1;
-                            // 容量计算
-                            tree.size += stat.size;
-                            tree.fileCount += stat.fileCount;
+                    return new Promise((resolve, reject) => {
+                        let promises = tree.folder.map(stat => {
+                            return search(stat);
                         });
-                        resolve(tree);
-                    })
-                });
-            }).then(() => {
-                return tree;
+                        Promise.all(promises).then(datas => {
+                            datas.map(stat => {
+                                // 文件夹计算
+                                if (stat && stat.isDirectory())  tree.folderCount += stat.folderCount + 1;
+                                // 容量计算
+                                if (stat) {
+                                    tree.size += stat.size;
+                                    tree.fileCount += stat.fileCount;
+                                }
+                            });
+                            resolve(tree);
+                        }).catch(err => {
+                            debugger;
+                        });
+                    });
             })
         }
-
     }
 
 }());
